2d57f0727da28f49d41fd7a91d9a59a2fa79faf2
chloecousin <chloe.m.cousin@gmail.com>
2024-05-01 11:10:55 -0400

    update project ov

--- Streamlit_app/Project_Overview.py
+++ Streamlit_app/Project_Overview.py
@@ -124,4 +124,4 @@ st.markdown("""
             After several simulations of external factors variations (aka if the temperatures increase by 10%...) we can see that the butterfly 
             population will suffer from poor external conditions. Simulations of external factors variations can be done on the app:
             """)
-st.page_link("Streamlit_app/pages/1_🦋_Model_App.py", label="Model App", icon="🦋")
+st.page_link("https://butterflies.streamlit.app/Model_App", label="Model App", icon="🦋")
